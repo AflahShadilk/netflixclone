0e45ec9725f1ae28b67f95cf35bb9f021fa26823
arogyamate <shadilvpm@gmail.com>
2025-06-14 18:07:58 +0530

    Initial commit

---
+++ netflix16/lib/domain/core/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const kWidth = SizedBox(width: 10,);
+const kHeight = SizedBox(height: 10,);
+
+const apikey ="76e12820615770c1b75520e3c1589321";
+
+const imagePath = "https://image.tmdb.org/t/p/w500";
